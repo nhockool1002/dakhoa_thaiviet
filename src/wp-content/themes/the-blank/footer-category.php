74b4eb3e0409ba041962f0b81ecb3cd3ca4d2b62
Nhut Nguyen Minh <nhut.nguyenminh.it@gmail.com>
2020-12-29 18:19:23 +0700

    Add blank 2

---
+++ src/wp-content/themes/the-blank/footer-category.php
@@ -0,0 +1,102 @@
+<div style="clear:both;"></div>
+<style>
+html {
+  margin-top: 0 !important;
+}
+.footer-wrapper {
+   background-color: #376CAE;
+}
+.footer-wrapper {
+  position: absolute;
+  right: 0;
+  left: 0;
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+.footerHeadline {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.wrapper-inner {
+  width: 1024px;
+  margin: 0 auto;
+}
+</style>
+<div class="tutorials-box">
+  <table>
+    <tr>
+      <td>
+        <div class="tut-icon">
+          <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+        </div>
+        <div class="phone-title-ft">Tư vấn miễn phí</div>
+        <div class="phone-headline-ft">028 XXX XXXX</div>
+      </td>
+      <td>
+        <div class="tut-icon">
+          <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+        </div>
+        <div class="head-appoint-ft">Đặt lịch khám</div>
+        <div class="apoint-headline-ft">ONLINE</div>
+      </td>
+      <td>
+        <div class="tut-icon">
+          <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+        </div>
+        <div class="head-date-ft">Mở cửa: 8h - 20h</div>
+        <div class="ct-date-ft">Tất cả các ngày trong tuần, kể cả ngày nghỉ lễ</div>
+      </td>
+      <td>
+        <div class="tut-icon">
+          <i class="fa fa-map" aria-hidden="true"></i>
+        </div>
+        <div class="head-map-ft">Chỉ đường</div>
+      </td>
+    </tr>
+  </table>
+</div>
+<div class="footer-wrapper">
+  <div class="wrapper-inner">
+  <table>
+    <tr>
+      <td class="footerHeadline">VỀ CHÚNG TÔI</td>
+      <td class="footerHeadline">LIÊN HỆ</td>
+      <td  class="footerHeadline"colspan="2">DIỆN BỆNH CHÍNH</td>
+    </tr>
+    <tr>
+      <td>
+        <ul>
+          <li><a href="#"> >> Giới thiệu</a></li>
+          <li><a href="#"> >> Đội ngũ bác sĩ</a></li>
+          <li><a href="#"> >> Cơ sở vật chất</a></li>
+          <li><a href="#"> >> Chỉ đường</a></li>
+        </ul>
+      </td>
+      <td>
+        <ul>
+          <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i> Phòng khám Đa Khoa</a></li>
+          <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i> Gmail</a></li>
+          <li><a href="#"><i class="fa fa-address-book" aria-hidden="true"></i> Chat ngay Zalo</a></li>
+          <li><a href="#"><i class="fa fa-phone-square" aria-hidden="true"></i> <span class="phone-footer-box">028 XXX XXXX</span></a></li>
+        </ul>
+      </td>
+      <td>
+        <ul>
+          <li><a href="#"> >> Bệnh lý bao quy đầu</a></li>
+          <li><a href="#"> >> Bệnh tiền liệt tuyến</a></li>
+          <li><a href="#"> >> Bệnh sùi mào gà</a></li>
+          <li><a href="#"> >> Bệnh giang mai</a></li>
+        </ul>
+      </td>
+      <td>
+        <ul>
+          <li><a href="#"> >> Bệnh lý tinh hoàn</a></li>
+          <li><a href="#"> >> Bệnh tiền liệt tuyến</a></li>
+          <li><a href="#"> >> Bệnh lậu</a></li>
+          <li><a href="#"> >> Mụn rộp sinh dục</a></li>
+        </ul>
+      </td>
+    </tr>
+  </table>
+  </div>
+</div>
